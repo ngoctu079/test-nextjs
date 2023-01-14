ff0a19f1f6db66c14185a70e7d656dae33a57b72
unknown <ngoc_tu@hydrax.io>
2023-01-14 11:07:20 +0700

    deploy

--- utils/axios/index.js
+++ utils/axios/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const fetchData = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL + "/api",
   timeout: 1000,
 });
