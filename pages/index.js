7642681b731d497cbfaadb65267f5b68de64d798
unknown <ngoc_tu@hydrax.io>
2023-01-14 12:01:42 +0700

    test SEO

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,20 @@ export default function Home() {
           content="NGỌC TÚ 1712858 NGỌC TÚ 1712858 NGỌC TÚ 1712858"
         />
       </Head>
-
+      <h1>NGỌC TÚ 1712858</h1>
+      Sinh viên chủ yếu là những người đăng ký vào trường hoặc cơ sở giáo dục
+      khác tham gia các lớp học trong khóa học để đạt được mức độ thành thạo môn
+      học theo hướng dẫn của người hướng dẫn, và dành thời gian bên ngoài lớp để
+      thực hiện bất kỳ hoạt động nào mà giảng viên chỉ định là cần thiết cho
+      việc chuẩn bị lớp học hoặc để gửi bằng chứng về sự tiến bộ đối với sự
+      thành thạo đó. Theo nghĩa rộng hơn, sinh viên là bất kỳ ai đăng ký chính
+      mình để được tham gia các khóa học trí tuệ chuyên sâu với một số chủ đề
+      cần thiết để làm chủ nó như là một phần của một số vấn đề ngoài thực tế
+      trong đó việc làm chủ các kiến thức như vậy đóng vai trò cơ bản hoặc quyết
+      định. Tại Vương quốc Anh và Ấn Độ, thuật ngữ "sinh viên" dành cho những
+      người đăng ký vào các trường trung học trở lên (ví dụ: cao đẳng hoặc đại
+      học); những người ghi danh vào trường tiểu học/trung học được gọi là "học
+      sinh."
       <h1>NGỌC TÚ 1712858</h1>
       <Link href="/posts">View all post</Link>
       <br />
